e851565147790ed8af1410727b7ae3a870764cf6
Lukas Gentele <17330150+LukasGentele@users.noreply.github.com>
2021-05-20 20:24:14 -0700

    open terminal for more control

--- devspace_start.sh
+++ devspace_start.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo "Installing maven dependencies"
+
 COLOR_CYAN="\033[0;36m"
 COLOR_RESET="\033[0m"
 
@@ -11,12 +13,14 @@ echo -e "${COLOR_CYAN}
   |____/ \___| \_/  |____/| .__/ \__,_|\___\___|
                           |_|
 ${COLOR_RESET}
-
 Welcome to your development container!
-This is how you can work with it:
-- ${COLOR_CYAN}Files will be synchronized${COLOR_RESET} between your local machine and this container
-- Certain files may be excluded from sync (see devspace.yaml)
 
-"
+This is how you can work with it:
+- Run \`${COLOR_CYAN}mvn spring-boot:run${COLOR_RESET}\` to run the application
+- Run \`${COLOR_CYAN}mvn package -T 1C -U -Dmaven.test.skip=true${COLOR_RESET}\` to build the application
+- ${COLOR_CYAN}Files will be synchronized${COLOR_RESET} between your local machine and this container (triggers build process)
+- Some ports will be forwarded, so you can access this container on your local machine via ${COLOR_CYAN}localhost${COLOR_RESET}:"
+
+devspace list ports
 
 bash
