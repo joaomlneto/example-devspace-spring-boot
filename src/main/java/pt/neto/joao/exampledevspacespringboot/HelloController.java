840d5dbad9079a1d48f8d3345f0c8f7a65be157a
Restyled.io <commits@restyled.io>
2021-05-24 13:27:08 +0000

    Restyled by clang-format

--- src/main/java/pt/neto/joao/exampledevspacespringboot/HelloController.java
+++ src/main/java/pt/neto/joao/exampledevspacespringboot/HelloController.java
@@ -5,8 +5,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
-    @RequestMapping("/")
-    public String index() {
-        return "Greetings from Spring Boot and DevSpace!";
-    }
+  @RequestMapping("/")
+  public String index() {
+    return "Greetings from Spring Boot and DevSpace!";
+  }
 }
